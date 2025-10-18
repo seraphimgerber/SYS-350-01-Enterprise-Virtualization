e396a9265901491fbd2c31639361739f55881597
seraphim <113275109+seraphimgerber@users.noreply.github.com>
2025-10-17 22:39:51 -0400

    Update menu2.py

--- menu2.py
+++ menu2.py
@@ -14,15 +14,25 @@ context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.verify_mode = ssl.CERT_NONE
 si = SmartConnect(host=config['vcenter_hostname'], user=config['username'], pwd=password, sslContext=context)
 
+# get vms list
+def get_vms(search=""):
+    container = si.content.viewManager.CreateContainerView(si.content.rootFolder, [vim.VirtualMachine], True)
+    vms = []
+    for vm in container.view:
+        if search == "" or search.lower() in vm.name.lower():
+            vms.append(vm)
+    return vms
+
 # hi menu!!
 menu = {}
-menu['1']="Power On/Off VMs"
-menu['2']="Snapshot VMs"
-menu['3']="Full Clone VM"
-menu['4']="Linked Clone VM"
-menu['5']="Delete VM"
-menu['6']="Create VM from Template"
-menu['7']="Exit"
+menu['1'] = "List VMs"
+menu['2'] = "Power On/Off"
+menu['3'] = "Snapshot"
+menu['4'] = "Full Clone"
+menu['5'] = "Linked Clone"
+menu['6'] = "Delete VM"
+menu['7'] = "Create from Template"
+menu['8'] = "Exit"
 while True:
     print("\n")
     for key in sorted(menu.keys()):
@@ -31,18 +41,34 @@ while True:
     choice = input("Choice: ")
 
     if choice == "1":
+        search = input("VM name: ")
+        vms = get_vms(search)
+        for vm in vms:
+            print(f"{vm.name} - {vm.runtime.powerState}")
 
     elif choice == "2":
+        search = input("VM name: ")
+        vms = get_vms(search)
+        action = input("1=On, 2=Off: ")
+        for vm in vms:
+            if action == "1":
+                vm.PowerOn()
+            else:
+                vm.PowerOff()
+            print(f"{vm.name} done")
 
     elif choice == "3":
+
 
     elif choice == "4":
 
     elif choice == "5":
 
     elif choice == "6":
-       
+
     elif choice == "7":
+       
+    elif choice == "8":
         Disconnect(si)
         break
 
